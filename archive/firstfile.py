903444037bb83ddecbc10c9af53ff337c46d6ab0
daiikin <gabriel.jouveaux@etu.unige.ch>
2024-10-21 17:21:23 +0200

    Including additionnal functions to the siggen_3102.py driver

--- archive/firstfile.py
+++ archive/firstfile.py
@@ -11,6 +11,28 @@ hi = 3
 #%%
 
 
+class sphere:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def volume(self):
+        return 4/3*3.14*self.radius**3
+    
+    def surface_area(self):
+        return 4*3.14*self.radius**2
+    
+    def __str__(self):
+
+        return f"Sphere with radius {self.radius}"
+    
+    def __add__(self, other):
+
+        return sphere(self.radius + other.radius)
+    
+    def __lt__(self, other):
+
+        return self.radius < other.radius
+
 #%%
 
 
--- drivers/siggen_3102.py
+++ drivers/siggen_3102.py
@@ -46,22 +46,54 @@ class DeepBlue():
             print("get_amplitude : Invalid reply from instrument")
             print(ans)
             return -1.0
-    
-###############################################################
+        
+    ### in Volt
+    def get_min_voltage(self, channel):
+        ans = self.query("SOUR{channel}:VOLT:LIM:LOW?")
+        try: 
+            ans = float(ans[:-1])
+            return ans
+        except:
+            print("get_min_voltage : Invalid reply from instrument")
+            print(ans)
+            return -1.0
+   
+   ### in Volt
+    def get_max_voltage(self, channel):
+        ans = self.query("SOUR{channel}:VOLT:LIM:HIGH?")
+        try: 
+            ans = float(ans[:-1])
+            return ans
+        except:
+            print("get_max_voltage : Invalid reply from instrument")
+            print(ans)
+            return -1.0
+        
+    def get_waveform(self, channel):
+        ans = self.query(f"SOUR{channel}:FUNC:SHAP?")
+        
 
+###############################################################
+    ### in Hertz
     def set_frequency(self, channel, frequency):
         self.write(f"SOURCE{channel}:FREQUENCY {frequency}")
         return
-
-
+    ### in Volt
     def set_amplitude(self,channel, amplitude):
         self.write(f"SOUR{channel}:AM {amplitude}")
         return
-    
-
-
-    def waveform(self):
-        pass
+    ### in Volt
+    def set_min_voltage(self, channel, min_voltage):
+        self.write(f"SOUR{channel}:VOLT:LIM:LOW {min_voltage}")
+        return
+    ### in Volt
+    def set_max_voltage(self, channel, max_voltage):
+        self.write(f"SOUR{channel}:VOLT:LIM:HIGH {max_voltage}")
+        return
+    ### Arguments can be "SINusoid" "SQUare" "PULSe" "RAMP" "PRNoise" "DC" "SINC" "GAUSsian" "LORentz" "ERISe" "EDECay""HAVersine"
+    def set_waveform(self, channel, waveform):
+        self.write(f"SOUR{channel}:FUNC:SHAP {waveform}")
+        return
     
     def __exit__(self, exc_type, exc_value, traceback):
         return
